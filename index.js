b9696950efcc6b08b5510f50d9b1f2837323ce50
Nithya <nithyaasworld@gmail.com>
2021-04-22 12:07:06 +0530

    handled error for the inner api

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-const stripe = require('stripe')('sk_test_oicXQM9cadHGTiJsQYGexfev');
-let readlineSync = require('readline-sync');
+//Problem statement: https://gist.github.com/McLarenCollege/ac4265921f271123e1a33ffa93f85cfd
+const stripe = require("stripe")("sk_test_oicXQM9cadHGTiJsQYGexfev");
+const readlineSync = require("readline-sync");
 
-let cardNumber = readlineSync.question('Enter your card number: ');
-let cardExpiryMonth = readlineSync.question('Enter card expiry month: ');
-let cardExpiryYear = readlineSync.question('Enter card expiry year: ');
-let cardCVCCode = readlineSync.question('Enter card CVC code: ');
+const cardNumber = readlineSync.question("Enter your card number: ");
+const cardExpiryMonth = readlineSync.question("Enter card expiry month: ");
+const cardExpiryYear = readlineSync.question("Enter card expiry year: ");
+const cardCVCCode = readlineSync.question("Enter card CVC code: ");
 
-stripe.tokens.create({
+stripe.tokens
+  .create({
     card: {
-        number: `${cardNumber}`,
-        exp_month: `${cardExpiryMonth}`,
-        exp_year: `${cardExpiryYear}`,
-        cvc: `${cardCVCCode}`,
+      number: `${cardNumber}`,
+      exp_month: `${cardExpiryMonth}`,
+      exp_year: `${cardExpiryYear}`,
+      cvc: `${cardCVCCode}`,
     },
-}).then((token) => {
-    console.log('Created card token with id: ',token.id);
-    stripe.charges.create({
+  })
+  .then((token) => {
+    console.log("Created card token with id: ", token.id);
+    stripe.charges
+      .create({
         amount: 360,
-        currency: 'usd',
+        currency: "usd",
         source: `${token.id}`,
-        description: 'My First Test Charge (created for API docs)',
-    })
-}).catch((err) => console.log(err.raw.message));
+        description: "My First Test Charge (created for API docs)",
+      })
+      .then((resp) => console.log("Created charge with id: ", resp.id))
+      .catch((err) => console.log(err.raw.message));
+  })
+  .catch((err) => console.log(err.raw.message));
 
 // Test Card Details to verify the above api
 // number: '4242424242424242',
